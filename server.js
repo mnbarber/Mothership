1521cf40ffd98dea8810a6f5f1ec4226a14272cc
megan <mnbarber1993@gmail.com>
2025-03-22 18:23:05 -0700

    please cors

--- server.js
+++ server.js
@@ -21,39 +21,16 @@ mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-  });
-
-let corsOptions = {
-  origin: ['https://mothership-0517ed1a863d.herokuapp.com']
-}
-
 // Middleware
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(logger('dev'));
-app.use(express.static(path.resolve(__dirname, './client/build')));
-
-app.all('*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
 
 // Routes
 app.use('/auth', authRouter);
 app.use('/test-jwt', testJwtRouter);
 app.use('/users', usersRouter);
 app.use('/posts', postsRouter);
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
-});
 
 // Start the server and listen on port 3000
 app.listen(3000, () => {
