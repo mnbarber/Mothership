2fe6483eaa4710f1a98dde77284d8a81cfeef751
megan <mnbarber1993@gmail.com>
2025-03-22 17:49:37 -0700

    i hate cors

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ app.use(express.static(path.resolve(__dirname, './client/build')));
 
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
